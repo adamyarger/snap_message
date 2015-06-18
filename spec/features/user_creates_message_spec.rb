3259a11b2c6e2d635ec9d3fd293df34803d5c053
adamyarger <yargeradam@gmail.com>
2015-06-18 16:50:32 -0500

    passing multi select test

--- spec/features/user_creates_message_spec.rb
+++ spec/features/user_creates_message_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'user creates a new message' do
 	scenario 'successfully' do
 		user = FactoryGirl.create(:user)
+		user2 = FactoryGirl.create(:user)
 		login_as(user, :scope => :user)
 		visit root_path
 
@@ -10,7 +11,10 @@ feature 'user creates a new message' do
 
 		fill_in 'Message', with: "hello world"
 		check('user1')
+		check('user2')
 		click_on 'submit'
+
+
 
 
 	end
