a444a6a60c03abda3c1e6e34b88ce6bd43ad5498
adamyarger <yargeradam@gmail.com>
2015-06-18 10:01:53 -0500

    passing association tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Message, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Message do
+	it 'has a valid factory' do
+		expect(build(:message)).to be_valid
+	end
+
+	it { should belong_to(:sender).class_name('User') }
+	it { should have_many(:recipients) }
+	it { should have_many(:users).through(:recipients) }
+	it { should validate_presence_of(:sender_id) }
+	it { should validate_presence_of(:body) }
 end
+
+
