8d74be3c8b80538ad8e05f59e771c99a429686c7
adamyarger <yargeradam@gmail.com>
2015-06-18 16:53:34 -0500

    done with message feature

--- spec/features/user_creates_message_spec.rb
+++ spec/features/user_creates_message_spec.rb
@@ -8,14 +8,12 @@ feature 'user creates a new message' do
 		visit root_path
 
 		click_on 'New'
-
 		fill_in 'Message', with: "hello world"
 		check('user1')
 		check('user2')
 		click_on 'submit'
 
 		expect(page.current_path).to eq root_path
-
-
+		expect(page).to have_content 'Message Sent!'
 	end
 end
