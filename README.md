873c042d0b19249908d9fd431720b390b1c15167
adamyarger <yargeradam@gmail.com>
2015-06-18 10:45:32 -0500

    adds seeds

---
+++ README.md
@@ -0,0 +1,1 @@
+In this screencast we will build a one to many private messaging system similar to that of snapchat
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(username:  "muffins",
+             email: "example@example.org",
+             password:              "gangstar1",
+             password_confirmation: "gangstar1")
+
+99.times do |n|
+  name  = Faker::Internet.user_name
+  email = "example-#{n+1}@example.org"
+  password = "password"
+  User.create!(username:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
