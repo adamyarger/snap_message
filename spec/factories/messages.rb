daf6294d128ffadca2df03b324aa82461f772d85
adamyarger <yargeradam@gmail.com>
2015-06-18 09:50:45 -0500

    changed factories

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :message do
     body "MyText"
-sender_id 1
+		sender
   end
-
 end
--- spec/factories/recipients.rb
+++ spec/factories/recipients.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :recipient do
-    message_id 1
-user_id 1
-read false
+    message
+		user
+		read false
   end
-
 end
